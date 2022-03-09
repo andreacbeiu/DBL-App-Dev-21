b183910223bde69ca93c13d5181457ad5a601e3f
Aryan Garg <aryangarg794@gmail.com>
2022-03-09 16:49:18 +0100

    added restaurant login, and possibly added firebase connection

--- app/src/main/java/com/example/ui_screens/CustomerLoginActivity.java
+++ app/src/main/java/com/example/ui_screens/CustomerLoginActivity.java
@@ -2,7 +2,11 @@ package com.example.ui_screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.ui_screens.ui.login.RegistrationActivity;
 
 public class CustomerLoginActivity extends AppCompatActivity {
 
@@ -10,6 +14,13 @@ public class CustomerLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_login);
+    }
+
+    public void openRegistration(View view) {
+        //sets the intent of the function: changing the activity
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        //starts the activity associated with the intent
+        startActivity(intent);
     }
 
 
