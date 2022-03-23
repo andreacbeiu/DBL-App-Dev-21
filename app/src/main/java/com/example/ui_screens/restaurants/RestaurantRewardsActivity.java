5c3dea336165686ccfa25c2a5fbe76b7c8ccfd5d
FranekHalwa <franciszekhalwa@gmail.com>
2022-03-23 11:58:33 +0100

    Started work on connecting the different restaurant pages.

---
+++ app/src/main/java/com/example/ui_screens/restaurants/RestaurantRewardsActivity.java
@@ -0,0 +1,15 @@
+package com.example.ui_screens.restaurants;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ui_screens.R;
+
+public class RestaurantRewardsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.restaurants_rewards_activity);
+    }
+}
