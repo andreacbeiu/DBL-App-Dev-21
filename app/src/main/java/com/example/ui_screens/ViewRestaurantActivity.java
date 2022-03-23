45ea1b6599c46986aee0ac6cd93de2afe701a82b
Hugo van Schalm <hugovanschalm@hotmail.com>
2022-03-23 20:11:30 +0100

    Added correct images to restaurant view

--- app/src/main/java/com/example/ui_screens/ViewRestaurantActivity.java
+++ app/src/main/java/com/example/ui_screens/ViewRestaurantActivity.java
@@ -2,10 +2,17 @@ package com.example.ui_screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class ViewRestaurantActivity extends AppCompatActivity {
 
@@ -24,5 +31,17 @@ public class ViewRestaurantActivity extends AppCompatActivity {
                         ((TextView)findViewById(R.id.tvViewRestaurantName)).setText(task.getResult().getData().get("name").toString());
                     }
                 });
+
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference().child("images/" + getIntent().getExtras().getString("id")+".jpg");
+        ImageView imageView = findViewById(R.id.ivViewRestaurant);
+
+        final long ONE_MEGABYTE = 1024*1024;
+        storageRef.getBytes(ONE_MEGABYTE * 2).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                imageView.setImageBitmap(bmp);
+            }
+        });
     }
 }
