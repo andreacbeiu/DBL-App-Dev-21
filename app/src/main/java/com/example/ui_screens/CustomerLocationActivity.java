22edc9a1caadbdfbe8b8d8285892128161092f8d
Ruxi <ruxi.hristache@yahoo.com>
2022-03-24 12:52:41 +0100

    dump attempt for gps

--- app/src/main/java/com/example/ui_screens/CustomerLocationActivity.java
+++ app/src/main/java/com/example/ui_screens/CustomerLocationActivity.java
@@ -1,14 +1,83 @@
 package com.example.ui_screens;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class CustomerLocationActivity extends Service {
+    LocationManager locationManager =
+            (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
-    @Nullable
-    
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+//
+//        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
+//                LOCATION_REFRESH_DISTANCE, mLocationListener);
+//    }
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//                                           int[] grantResults) {
+//        switch (requestCode) {
+//            case PERMISSION_REQUEST_CODE:
+//                // If request is cancelled, the result arrays are empty.
+//                if (grantResults.length > 0 &&
+//                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    // Permission is granted. Continue the action or workflow
+//                    // in your app.
+//                }  else {
+//                    // Explain to the user that the feature is unavailable because
+//                    // the features requires a permission that the user has denied.
+//                    // At the same time, respect the user's decision. Don't link to
+//                    // system settings in an effort to convince the user to change
+//                    // their decision.
+//                }
+//                return;
+//        }
+//        // Other 'case' lines to check for other
+//        // permissions this app might request.
+//    }
+
+//    private Boolean isLocationPermissionGranted() {
+//
+//        if (ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+//
+//            ActivityCompat.requestPermissions( this, new String[] {  android.Manifest.permission.ACCESS_COARSE_LOCATION  },
+//                    LocationService.MY_PERMISSION_ACCESS_COURSE_LOCATION );
+//        }
+//
+//        if (ActivityCompat.checkSelfPermission(
+//                this,
+//                android.Manifest.permission.ACCESS_COARSE_LOCATION
+//        ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+//                this,
+//                android.Manifest.permission.ACCESS_FINE_LOCATION
+//        ) != PackageManager.PERMISSION_GRANTED
+//        ) {
+//            ActivityCompat.requestPermissions(
+//                    this,
+//                    arrayOf(
+//                            android.Manifest.permission.ACCESS_FINE_LOCATION,
+//                            android.Manifest.permission.ACCESS_COARSE_LOCATION
+//                    ),
+//                    requestcode
+//            )
+//            return false;
+//        } else {
+//            return true;
+//        }
+//    }
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
