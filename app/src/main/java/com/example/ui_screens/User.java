662bad8281a9552832db61d596b2664d04c0ffc9
Aryan Garg <aryangarg794@gmail.com>
2022-03-24 20:38:25 +0100

    added user creation functionality, user can be created in the registration page, cannot login yet.

---
+++ app/src/main/java/com/example/ui_screens/User.java
@@ -0,0 +1,18 @@
+package com.example.ui_screens;
+
+public class User {
+
+    public String email, password;
+    public Integer phone;
+
+
+    public User() {
+
+
+    }
+
+    public User(String email, Integer phone) {
+        this.email = email;
+        this.phone = phone;
+    }
+}
