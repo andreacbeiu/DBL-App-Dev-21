c03abf3b9287c54f5219baab398d32250edd63a5
andreacbeiu <a.c.beiu@student.tue.nl>
2022-03-26 02:31:12 +0100

    Booking functionality updated
    updated the sequence of pop-ups when a customer tries to book at a restaurant

--- app/src/main/java/com/example/ui_screens/ViewRestaurantActivity.java
+++ app/src/main/java/com/example/ui_screens/ViewRestaurantActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,6 +26,11 @@ import java.util.Calendar;
 
 public class ViewRestaurantActivity extends AppCompatActivity implements View.OnClickListener {
 
+    int mYear, mMonth, mDay, mHour, mMinute;
+    String preferreddate, preferredtime, str_nrpeople, str_message;
+    EditText nrpeople, message;
+    TextView map;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +39,12 @@ public class ViewRestaurantActivity extends AppCompatActivity implements View.On
         //selecting book button + adding click listener
         Button bookbutton = findViewById(R.id.bookbutton);
         bookbutton.setOnClickListener(this);
+        preferreddate = "25-03-2022";
+        preferredtime = "23:51";
+        str_nrpeople = "0";
+        str_message = "";
+        map = (TextView) findViewById(R.id.textView16);
+
 
         String restaurantId = getIntent().getExtras().getString("id");
         FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -74,67 +87,82 @@ public class ViewRestaurantActivity extends AppCompatActivity implements View.On
     }
 
 
-    //creates a dialog window when bookbutton is pressed
+    //creates a date-picking dialog window when a button is pressed
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.bookbutton:
-                pickTimeDialog();
-                break;
-        }
+        pickDateDialog();
     }
 
 
-    //dialog window builder function for bookbutton
-    public void pickTimeDialog() {
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        dialog.setTitle("Book a Table");
+    //date-picking dialog builder
+    public void pickDateDialog() {
 
-        //set layout of the dialog pop upb
-        dialog.setView(R.layout.dialogtimepicklayout);
+        //Get Current Date + set day, month, year vars
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
 
-        //sets positive button
-        dialog.setPositiveButton("CONTINUE",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog,
-                                        int which) {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
 
-                        //get input values for nr of people + date + message
-                        String str_date = getDate(R.id.date).toString();
-                        String str_time = getTime(R.id.time).toString();
-                        confirmBookingDialog();
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+
+                        preferreddate = dayOfMonth + "-" + (monthOfYear + 1) + "-" + year;
+                        //txtDate.setText(preferreddate);
+                        //Toast.makeText(getApplicationContext(),"You have made a booking!",Toast.LENGTH_LONG).show();
+                        pickTimeDialog();
 
                     }
-                });
-
-        //sets negative button
-        dialog.setNegativeButton("CANCEL",new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(getApplicationContext(),"You have cancelled a booking!",Toast.LENGTH_LONG).show();
-            }
-        });
-        //create the dialog pop-up
-        AlertDialog bookingDialog = dialog.create();
-        bookingDialog.show();
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
     }
 
+    //time-picking dialog builder
+    public void pickTimeDialog() {
+        // Get Current Time
+        final Calendar c = Calendar.getInstance();
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+
+        // Launch Time Picker Dialog
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay,
+                                          int minute) {
+
+                        preferredtime = hourOfDay + ":" + minute;
+                        confirmBookingDialog();
+                    }
+                }, mHour, mMinute, false);
+        timePickerDialog.show();
+    }
+
+    //confirms the booking via asking for nr of people and optional message
     public void confirmBookingDialog() {
+        //builds alert dialog
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         dialog.setTitle("Book a Table");
-
-        //set layout of the dialog pop upb
-        dialog.setView(R.layout.dialogbooklayout);
+        //view inflater
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.dialogbooklayout, null, false);
+        nrpeople = (EditText) dialogView.findViewById(R.id.nrpeople);
+        message = (EditText) dialogView.findViewById(R.id.message);
+        //set layout of the dialog pop up
+        dialog.setView(dialogView);
 
         //sets positive button
         dialog.setPositiveButton("BOOK",
                 new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog,
-                                        int which) {
-                        Toast.makeText(getApplicationContext(),"You have booked a table!",Toast.LENGTH_LONG).show();
-                        //get input values for nr of people + date + message
-                        int int_nrpeople = getNrPeople(R.id.nrpeople);
-                        String str_message = getMessage(R.id.message);
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        str_nrpeople = nrpeople.getText().toString();
+                        str_message = message.getText().toString();
+
+                        Toast.makeText(getApplicationContext(), str_message, Toast.LENGTH_LONG).show();
+
                     }
                 });
 
@@ -149,48 +177,6 @@ public class ViewRestaurantActivity extends AppCompatActivity implements View.On
         AlertDialog bookingDialog = dialog.create();
         bookingDialog.show();
     }
-
-
-    public int getNrPeople (int id) {
-        EditText nrpeople = (EditText) findViewById(id);
-        String str_nrpeople = nrpeople.getText().toString();
-        int int_nrpeople = Integer.parseInt(str_nrpeople);
-        return int_nrpeople;
-    }
-
-    public String getMessage (int id) {
-        EditText message = (EditText) findViewById(id);
-        String str_message = message.getText().toString();
-        return str_message;
-    }
-
-    public String getDate (int id) {
-        DatePicker date = (DatePicker) findViewById(id);
-        int day = date.getDayOfMonth ();
-        int month = date.getMonth();
-        int year = date.getYear();
-        String str_date = day + "/" + (month + 1) + "/" + year;
-        return str_date;
-    }
-
-    public String getTime (int id) {
-        TimePicker time = (TimePicker) findViewById(id);
-        int hour = 0;
-        int minute = 0;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
-            hour = time.getHour();
-        }
-        else { hour = time.getCurrentHour(); }
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
-            minute = time.getMinute();
-        }
-        else { minute = time.getCurrentMinute(); }
-        String str_time = minute + ":" + hour;
-        return str_time;
-    }
-
-
-
 
 
 }
