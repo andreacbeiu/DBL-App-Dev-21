24d27515b1ceca954390688ff60f99cb65b60d37
FranekHalwa <franciszekhalwa@gmail.com>
2022-04-06 18:54:56 +0200

    Edit table page connected to add button on tables page for restaurant and added to manifest.

--- app/src/main/java/com/example/ui_screens/restaurants/RestaurantTablesActivity.java
+++ app/src/main/java/com/example/ui_screens/restaurants/RestaurantTablesActivity.java
@@ -1,6 +1,8 @@
 package com.example.ui_screens.restaurants;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,12 @@ public class RestaurantTablesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.restaurant_tables_activity);
+    }
+
+    public void openEditTable(View view) {
+        //sets the intent of the function: changing the activity
+        Intent intent = new Intent(this, TableEditActivity.class);
+        //starts the activity associated with the intent
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/ui_screens/restaurants/TableEditActivity.java
@@ -0,0 +1,15 @@
+package com.example.ui_screens.restaurants;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ui_screens.R;
+
+public class TableEditActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.restaurant_edit_table);
+    }
+}
